3369b720cd68921b813a08b861f0b800760517a6
ogrezem <ogrezem@gmail.com>
2019-05-07 13:57:26 +0300

    123

---
+++ src/main/java/ru/ogrezem/codeWarsSolution/domain/discordApi/commands/ShowCustomersCommand.java
@@ -0,0 +1,44 @@
+package ru.ogrezem.codeWarsSolution.domain.discordApi.commands;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.ogrezem.codeWarsSolution.domain.jts.Customer;
+import ru.ogrezem.codeWarsSolution.domain.jts.CustomerRepository;
+
+import java.util.List;
+
+@Service
+public class ShowCustomersCommand extends Command {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public ShowCustomersCommand() {
+        this.name = "show";
+        this.help = "shows some customers";
+        this.arguments = "<args>";
+        this.guildOnly = false;
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        if (event.getArgs().equals("all")) {
+            var responseStringBuilder = new StringBuilder()
+                    .append("Список всех customer: \n");
+            if (customerRepository.count() == 0) {
+                event.reply("База customers пуста");
+                return;
+            }
+            List<Customer> customersList = customerRepository.findAll();
+            for (Customer customer : customersList) {
+                responseStringBuilder.append(customer.toString())
+                        .append("\n");
+            }
+            event.reply(responseStringBuilder.toString());
+        } else {
+            event.reactError();
+        }
+    }
+}
