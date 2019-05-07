c33c01b6aacc998dcf4f33e1a30e3a119012aacd
ogrezem <ogrezem@gmail.com>
2019-05-07 18:43:45 +0300

    123

--- src/main/java/ru/ogrezem/codeWarsSolution/domain/jts/Customer.java
+++ src/main/java/ru/ogrezem/codeWarsSolution/domain/jts/Customer.java
@@ -5,7 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @ToString
-@Document("customer")
+@Document("customers")
 public class Customer {
 
     @Id
